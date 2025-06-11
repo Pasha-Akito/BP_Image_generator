efdacebffb93ed3969a986707df5943f8551e276
Pasha-Akito <1masterpasha@gmail.com>
2025-06-11 20:01:08 +0100

    removed second inside

--- properties.py
+++ properties.py
@@ -2,7 +2,7 @@ PROPERTY_VOCAB = {
     # Basic functions
     "LEFT": 0, "RIGHT": 1, "EXISTS": 2, "EXACTLY": 3, "GREATERLA": 4,
     "MORESIMLA": 5, "GREATERLLA": 6, "MORE": 7, "EQUALNUM": 8, "GET": 9,
-    "CAP": 10, "SETMINUS": 11, "CUP": 12, "INSIDE": 13, "CONTAINS": 14,
+    "CAP": 10, "SETMINUS": 11, "CUP": 12, "INSIDE": 13,
     "ALIGNED": 15, "HIGH": 16, "LOW": 17,
     # New Additions
     "CEASES": 41, # Opposite of EXISTS
@@ -41,9 +41,9 @@ PROPERTY_VOCAB = {
     "LEFT": 49, "RIGHT": 50, "UP": 51, "DOWN": 52,
     "MIDDLE": 53, "START": 54, "END": 55,
     "CLOCKWISE": 56, "COUNTERCLOCKWISE": 57,
-    "INSIDE": 65, "OUTSIDE": 66,
+    "OUTSIDE": 66,
     "INCREASING": 68, "DECREASING": 69,
-    "ON": 70, # Something is on something
+    "ON": 65, # Something is on something
     "MOVING": 58 # Uses one of the above 
 
 

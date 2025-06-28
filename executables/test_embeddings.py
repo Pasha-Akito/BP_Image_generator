bb95557f49f4b8dffc7e38946192c9488529ddf0
Pasha-Akito <1masterpasha@gmail.com>
2025-06-28 18:29:44 +0100

    Added cosine similarity from Clip ViT-B/32 model

--- executables/test_embeddings.py
+++ executables/test_embeddings.py
@@ -50,7 +50,7 @@ def clip_similarity(image_path, text, title):
         cosine_similarity = ((image_embeddings @ text_embeddings.t())).item()
         print(f"CLIP | {title} | Cosine Similarity: {cosine_similarity}")
 
-def clip_similarity_two_images(image_path, image_2_path, title):
+def clip_similarity_two_images(image_path, image_2_path):
     image = load_and_transform_image(IMAGE_SIZE, image_path).to(device)
     image = image.unsqueeze(0)
     image = F.interpolate(image, size=(224, 224), mode='bilinear', align_corners=False)
@@ -70,8 +70,20 @@ def clip_similarity_two_images(image_path, image_2_path, title):
         image_2_embeddings = clip_model.encode_image(image_2)
         image_2_embeddings = image_2_embeddings / image_2_embeddings.norm(dim=-1, keepdim=True)
         
-        cosine_similarity = ((image_embeddings @ image_2_embeddings.t())).item()
-        print(f"CLIP | {title} | Cosine Similarity: {cosine_similarity}")
+        return ((image_embeddings @ image_2_embeddings.t())).item()
+
+def get_clip_cosine_similarity_of_bongard_problem(bp_number):
+    bp_folder_name = extract_folder_name(bp_number)
+    image_paths_for_bp = [f"../bp_images/{bp_folder_name}/{i}.png" for i in range(12)]
+    image_pair_matrix = [[img_path, image_path2] for img_path in image_paths_for_bp for image_path2 in image_paths_for_bp]
+    flat_cosine_similarities = [clip_similarity_two_images(pair[0], pair[1]) for pair in image_pair_matrix]
+    similarity_matrix = [flat_cosine_similarities[i * 12 : (i + 1) * 12] for i in range(12)]
+    return similarity_matrix
+
+def save_clip_cosine_similarity_of_first_100_bongard_problems():
+    for i in range(100):
+        similarity_matrix = get_clip_cosine_similarity_of_bongard_problem(i + 1)
+        create_heatmap_of_cosine_matrix(similarity_matrix, i + 1, "CLIP")
         
 def get_vgg_cosine_similarity(image_path, image_2_path):
     image = load_and_transform_image(IMAGE_SIZE, image_path).to(device)
@@ -90,50 +102,39 @@ def get_vgg_cosine_similarity_of_bongard_problem(bp_number):
     similarity_matrix = [flat_cosine_similarities[i * 12 : (i + 1) * 12] for i in range(12)]
     return similarity_matrix
 
-def create_heatmap_of_cosine_matrix(similarity_matrix, bp_number):
+
+def create_heatmap_of_cosine_matrix(similarity_matrix, bp_number, title):
     plt.figure(figsize=(10, 8))
     image_labels = ["left1","left2","left3","left4","left5","left6","right1","right2","right3","right4","right5","right6"]
     percentage_annotation_matrix = [[f"{cosine_similarity * 100:.0f}" for cosine_similarity in row] for row in similarity_matrix]
-    ax = sns.heatmap(
-        similarity_matrix, 
-        xticklabels=image_labels, 
-        yticklabels=image_labels,
-        annot=percentage_annotation_matrix,
-        fmt="",
-        square=True,
-        cmap="inferno",
-        vmin=0, 
-        vmax=1,
-        cbar_kws={'ticks': [0, 0.5, 1]},
-        annot_kws={'size': 10}
-        )
+    ax = sns.heatmap(similarity_matrix, xticklabels=image_labels, yticklabels=image_labels,annot=percentage_annotation_matrix,
+        fmt="",square=True,cmap="inferno",vmin=0, vmax=1,cbar_kws={'ticks': [0, 0.5, 1]},annot_kws={'size': 10})
     cbar = ax.collections[0].colorbar
     cbar.set_ticklabels(['0%', '50%', '100%'])
     plt.xticks(rotation=45)
     plt.yticks(rotation=0)
-    plt.title(f"VGG Similarity | Bongard Problem {bp_number}", fontsize=16, pad=20)
+    plt.title(f"{title} Similarity | Bongard Problem {bp_number}", fontsize=16, pad=20)
     ax.tick_params(axis='both',labelsize=10)
     ax.axvline(x=6, linewidth=1, color="white")
     ax.axhline(y=6, linewidth=1, color="white")
     plt.tight_layout()
-    plt.savefig(f'../cosine_similarity/VGG/similarity_heatmap_bongard_problem_{bp_number}.png', dpi=300)
+    plt.savefig(f'../cosine_similarity/{title}/similarity_heatmap_bongard_problem_{bp_number}.png', dpi=300)
     print(f"Saved similarity_heatmap_bongard_problem_{bp_number}.png'")
     plt.close()
 
-def save_cosine_similarity_of_first_100_bongard_problems():
+def save_vgg_cosine_similarity_of_first_100_bongard_problems():
     for i in range(100):
         similarity_matrix = get_vgg_cosine_similarity_of_bongard_problem(i + 1)
-        create_heatmap_of_cosine_matrix(similarity_matrix, i + 1)
+        create_heatmap_of_cosine_matrix(similarity_matrix, i + 1, "VGG")
 
 if __name__ == "__main__":
     bp37_left_image_path = "../bp_images/p037/0.png"
     bp37_right_image_path = "../bp_images/p037/11.png" 
 
-    get_vgg_cosine_similarity(bp37_left_image_path, bp37_right_image_path)
-    clip_similarity_two_images(bp37_left_image_path, bp37_right_image_path, "BP37 Opposing Sides")
     clip_similarity(bp37_left_image_path, "LEFT(GREATERLLA(TRIANGLES,CIRCLES,YPOS))", "BP37 Left Image Correct Text")
     clip_similarity(bp37_right_image_path, "LEFT(GREATERLLA(TRIANGLES,CIRCLES,YPOS))", "BP37 Right Image Correct Text")
     clip_similarity(bp37_left_image_path, "RIGHT(LESSSIMLA(FIGURES,SIZE))", "BP37 Left Image Wrong Text")
     clip_similarity(bp37_right_image_path, "RIGHT(LESSSIMLA(FIGURES,SIZE))", "BP37 Right Image Wrong Text")
 
-    save_cosine_similarity_of_first_100_bongard_problems()
+    # save_vgg_cosine_similarity_of_first_100_bongard_problems()
+    save_clip_cosine_similarity_of_first_100_bongard_problems()
